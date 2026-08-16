4b7bdd4bc96f1933c02004ba56127b1af46a3687
whosmitri <hellomitrii@gmail.com>
2026-08-16 10:43:58 -0300

    feat: adiciona função de abrir arquivos PDF

--- o_leitor/src/main.py
+++ o_leitor/src/main.py
@@ -1,6 +1,26 @@
 import flet as ft
 
 def main(page: ft.Page):
+
+    # handler = lida com eventos
+    # "e" = sigla comum para "evento"/"event"
+    async def picking_file(e):
+
+        # 1. abrir o seletor;
+        # 2. esperar o usuário escolher
+        # 3. coloque o resultados em file_selected
+
+        selected_files = await ft.FilePicker().pick_files(allow_multiple=False, allowed_extensions=["pdf"])
+
+        # verifica se há arquivos selecionados
+        if selected_files:
+            file_selected = selected_files[0]
+            print(file_selected.path)
+            print(file_selected.name)
+        # se não tem nada, apenas escreve no terminal que foi cancelado
+        else:
+            print("Canceled!!!")
+
     # título da página
     page.title = "O Leitor"
 
@@ -21,8 +41,9 @@ def main(page: ft.Page):
 
     # botão para selecionar o arquivo
     btn_add_file =ft.Button(
-        "Escolher PDF",
-        icon=ft.Icons.FOLDER_OPEN
+        content="Escolher PDF",
+        icon=ft.Icons.FOLDER_OPEN,
+        on_click=picking_file
     )
 
     # adicionandos os componentes na página
